a8784d9489c92f4df351ac4eea4840da2845f903
Kevin A. Archie <karchie@alumni.caltech.edu>
2012-01-19 07:05:26 -0600

    use Ivy for dependencies

--- liverepl.sh
+++ liverepl.sh
@@ -3,13 +3,20 @@
 
 [ -z "$JDK_HOME" ] && JDK_HOME=/usr/lib/jvm/default-java
 LIVEREPL_HOME="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-CLOJURE_JAR="$LIVEREPL_HOME/clojure.jar"
+
+if [ "x" != "x$CLASSPATH" ]; then
+CLASSPATH="${CLASSPATH}:"
+fi
+CLASSPATH="${CLASSPATH}`find \"$LIVEREPL_HOME\" -name '*.jar' -print | tr '\n' :`"
 
 if [ ! -f "$JDK_HOME/lib/tools.jar" ]; then
-   echo 'Unable to find $JDK_HOME/lib/tools.jar'
-   echo "Please set the JDK_HOME environment variable to the location of your JDK."
-   exit 1
+    echo 'Unable to find $JDK_HOME/lib/tools.jar'
+    echo "Please set the JDK_HOME environment variable to the location of your JDK."
+    exit 1
+else
+    CLASSPATH="${CLASSPATH}${JDK_HOME}/lib/tools.jar"
 fi
 
-java -cp "$LIVEREPL_HOME/liverepl-agent.jar:$JDK_HOME/lib/tools.jar" net.djpowell.liverepl.client.Main "$CLOJURE_JAR" "$LIVEREPL_HOME/liverepl-agent.jar" "$LIVEREPL_HOME/liverepl-server.jar" "$@"
+echo Running with classpath $CLASSPATH
+java -cp $CLASSPATH net.djpowell.liverepl.client.Main "$CLOJURE_JAR" "$LIVEREPL_HOME/liverepl-agent.jar" "$LIVEREPL_HOME/liverepl-server.jar" "$@"
 
